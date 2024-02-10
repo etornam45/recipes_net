f8b459d00237352bdbaa2ec9a522229682252091
etornam45 <navidben45@gmail.com>
2024-02-10 09:05:56 +0000

    Added appropraite states

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -9,3 +9,9 @@ export const data_store = writable<Store>({
 });
 
 export const is_all_selected = writable<boolean | null>(null);
+
+export const generating = writable<boolean>(true)
+
+export const user_email = writable<string | null>(null)
+
+export const showing_form = writable<boolean>(false)
